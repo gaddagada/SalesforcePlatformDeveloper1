4aadee09e18985029975830b7fe14da8f34739fc
gaddagada <gayathri.addagada@gmail.com>
2025-08-15 09:23:25 -0500

    Refactored child-to-pareant communication in LWC to use programmatic eventhandling via addEventListener instead of declarative HTML markup

--- force-app/main/default/lwc/parentComponent2/parentComponent2.js
+++ force-app/main/default/lwc/parentComponent2/parentComponent2.js
@@ -2,10 +2,15 @@ import { LightningElement, track } from 'lwc';
 
 export default class ParentComponent2 extends LightningElement {
     @track msg; 
+    
+    constructor(){
+        super(); 
+        this.templatae.addEventListener('mycustomevent', this.handleCustomEvent.bind(this)); 
+    }
     handleCustomEvent(event){
         // In the parent component we are calling the child component we have added this on event listener whatever is 
         // in the detail of that event store it in this property and it is a trackable property. So anytime the value 
         // changes, it is going to render the component.
         this.msg = event.detail;
     }
-}
+} 
