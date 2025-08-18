ced4e963218cf45f71910246b5a114c8e3e814e4
gaddagada <gayathri.addagada@gmail.com>
2025-08-17 22:54:38 -0500

    Implemented full LWC navigation using NavigationMixin to handle record pages (new/edit/view), object home pages, standard named pages (home/chatter), and external web pages via dynamic page references.

--- force-app/main/default/lwc/navigationServiceLWCDemo/navigationServiceLWCDemo.js
+++ force-app/main/default/lwc/navigationServiceLWCDemo/navigationServiceLWCDemo.js
@@ -1,7 +1,7 @@
-import { LightningElement, api } from 'lwc';
-import {NavigationMixin} from 'lightning/navigation';
-export default class NavigationServiceLWCDemo extends LightningElement {
-    @api recordId; 
+import { LightningElement,api } from 'lwc';
+import {NavigationMixin} from 'lightning/navigation'
+export default class NavigationServiceLWCDemo extends NavigationMixin(LightningElement) {
+    @api recordId;
     navigateToNewRecordPage(){
         this[NavigationMixin.Navigatae]({
             type:'standard__recordPage', 
@@ -31,5 +31,74 @@ export default class NavigationServiceLWCDemo extends LightningElement {
                 "actionName":"view"
             }
         })
+    }
+
+    navigateAccRecentView(){
+        this[NavigationMixin.Navigate]({
+            "type":"standard__objectPage", 
+            "attributes":{
+                "objectApiName":"Account", 
+                "actionName":"list"
+            }, 
+            "state":{
+                "filterName":"Recent"
+            }
+        })
+    }
+    navigateRelatedListView(){
+        this[NavigationMixin.Navigate]({
+            "type":"standard__recordRelationshipPage",
+            "attributes":{
+                recordId:this.recordId,
+                objectApiName:'Account',
+                relationshipApiName:'Contacts',
+                actionName:'view'
+            }
+        });
+    }
+
+    navigateAccObject(){
+        this[NavigationMixinn.Navigate]({
+            "type":"standard_objectPage", 
+            "attributes":{
+                "objectApiName":"Account", 
+                "actionName":"home"
+            }
+        });
+    }
+    navigateConObject(){
+        this[NavigationMixinn.Navigate]({
+            "type":"standard_objectPage", 
+            "attributes":{
+                "objectApiName":"Contact", 
+                "actionName":"home"
+            }
+        });
+    }
+    navigateToWebPage(){
+        this[NavigationMixin.Navigate]({
+            "type":"standard__webPage",
+            "attributes":{
+                "url":"https://www.mytutorialrack.com/"
+            }
+        });
+    }
+
+    navigateToHomePage(){
+        this[NavigationMixin.Navigate]({
+            type:'standard__namedPage',
+            attributes:{
+                pageName:'home'
+            },
+        });
+    }
+    navigateToChatterHome()
+    {
+        this[NavigationMixin.Navigate]({
+            type:'standard__namedPage',
+            attributes:{
+                pageName:'chatter'
+            },
+        });
     }
 }
