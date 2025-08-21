f336e1882beef71c852d0c54e0df0c0e18f7bbf5
gaddagada <gayathri.addagada@gmail.com>
2025-08-21 11:06:44 -0500

    Implemented parent-to-child communication in LWC using public `@api` property. Parent component iterates over a list of employee objects and passes each to the child via a custom attribute, rendering individual employee tiles dynamically.

--- force-app/main/default/lwc/employeeDetails/employeeDetails.js
+++ force-app/main/default/lwc/employeeDetails/employeeDetails.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class EmployeeDetails extends LightningElement {}
+export default class EmployeeDetails extends LightningElement {
+    empDetails=[
+        {
+            empName:'Steve', empAddress:'NC'
+        },
+        {
+            empName:'Smith', empAddress:'TX'
+        },  {
+            empName:'Rodger', empAddress:'LA'
+        },  {
+            empName:'John', empAddress:'SC'
+        },  {
+            empName:'Raymond', empAddress:'Washington, DC'
+        }
+    ]
+}
